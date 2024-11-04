69c795d0bd9ae468d4eb6e29579e280c8c2c5684
dvenkatesan-tmx <dharshan.venkatesan@tmx.com>
2024-11-03 23:28:32 -0500

    Update dag1.py

--- dags/dag1.py
+++ dags/dag1.py
@@ -1,1 +1,45 @@
-#check build
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+# Function to be executed by PythonOperator
+def print_hello():
+    print("Hello, Airflow!")
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 11, 3),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Instantiate the DAG
+dag = DAG(
+    'sample_dag',
+    default_args=default_args,
+    description='A simple sample DAG',
+    schedule_interval=timedelta(days=1),  # Runs once a day
+)
+
+# Define the tasks
+start_task = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+hello_task = PythonOperator(
+    task_id='print_hello',
+    python_callable=print_hello,
+    dag=dag,
+)
+
+end_task = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+# Set task dependencies
+start_task >> hello_task >> end_task
